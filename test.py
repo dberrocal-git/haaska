3fadc1240d2e32cb369b542decf9a0c0eeceef83
Malachi Soord <inverse.chi@gmail.com>
2025-10-21 22:41:06 +0200

    chore: restructure haaska

--- test.py
+++ test.py
@@ -1,23 +1,26 @@
 import os
+
 import pytest
 
-from haaska import HomeAssistant, Configuration
+from haaska import Configuration, HomeAssistant, SessionFactory
 
 
 @pytest.fixture
 def configuration():
-    return Configuration(opts_dict={
-        "url": "http://localhost:8123",
-        "bearer_token": "",
-        "debug": False,
-        "ssl_verify": True,
-        "ssl_client": []
-    })
+    return Configuration(
+        {
+            "url": "http://localhost:8123",
+            "bearer_token": "",
+            "debug": False,
+            "ssl_verify": True,
+            "ssl_client": [],
+        }
+    )
 
 
 @pytest.fixture
 def home_assistant(configuration):
-    return HomeAssistant(configuration)
+    return HomeAssistant(configuration.url, SessionFactory.create_session(configuration))
 
 
 def test_ha_build_url(home_assistant):
@@ -25,9 +28,9 @@ def test_ha_build_url(home_assistant):
     assert url == "http://localhost:8123/api/test"
 
 
-def test_get_user_agent(home_assistant):
+def test_session_factory_user_agent(configuration):
     os.environ["AWS_DEFAULT_REGION"] = "test"
-    user_agent = home_assistant.get_user_agent()
+    user_agent = SessionFactory.create_session(configuration).headers["User-Agent"]
     assert user_agent.startswith("Home Assistant Alexa Smart Home Skill - test - python-requests/")
 
 
@@ -38,10 +41,7 @@ def test_config_get(configuration):
 
 
 def test_config_get_url(configuration):
-    test_urls = [
-        "http://hass.example.com:8123",
-        "http://hass.example.app"
-    ]
+    test_urls = ["http://hass.example.com:8123", "http://hass.example.app"]
     for expected_url in test_urls:
         assert configuration.get_url(expected_url + "/") == expected_url
         assert configuration.get_url(expected_url + "/api") == expected_url
